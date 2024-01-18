d3b30a84c10063f87c9dd54b42b167fd2be04635
Nisa Alivia <nisa.alivia28@gmail.com>
2024-01-18 12:20:10 +0700

    Consolidate routes in index.js and enable database sync for models

--- inventory-app-api/index.js
+++ inventory-app-api/index.js
@@ -2,9 +2,22 @@ import express from "express"
 import cors from "cors"
 import session from "express-session"
 import dotenv from "dotenv"
+// import db from "./config/Database.js"
+import UserRoute from "./routes/UserRoute.js"
+import CategoryRoute from "./routes/CategoryRoute.js"
+import UnitRoute from "./routes/UnitRoute.js"
+import TypeRoute from "./routes/TypeRoute.js"
+import GoodsRoute from "./routes/GoodsRoute.js"
+import TransactionRoute from "./routes/TransactionRoute.js"
+import TransactionDetailRoute from "./routes/TransactionDetailRoute.js"
+
 dotenv.config()
 
 const app = express()
+
+// (async() => {
+//     await db.sync()
+// })()
 
 app.use(session({
     secret: process.env.SESS_SECRET,
@@ -21,6 +34,13 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use(UserRoute)
+app.use(CategoryRoute)
+app.use(UnitRoute)
+app.use(TypeRoute)
+app.use(GoodsRoute)
+app.use(TransactionRoute)
+app.use(TransactionDetailRoute)
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server up and running...')
