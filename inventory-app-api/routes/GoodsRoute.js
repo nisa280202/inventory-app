4b715865de2353de5bd9627eed741205174bf85d
Nisa Alivia <nisa.alivia28@gmail.com>
2024-01-19 18:45:15 +0700

    Integrate upload middleware with 'images' single file handling in user and goods routes

--- inventory-app-api/routes/GoodsRoute.js
+++ inventory-app-api/routes/GoodsRoute.js
@@ -7,13 +7,14 @@ import {
     deleteGoods 
 } from "../controllers/Goods.js"
 import { verifyUser, officeStaffOnly } from "../middleware/AuthUser.js"
+import upload from "../middleware/Upload.js"
 
 const router = express.Router()
 
 router.get('/goods', verifyUser, getGoods)
 router.get('/goods/:id', verifyUser, getGoodsById)
-router.post('/goods', verifyUser, officeStaffOnly, createGoods)
-router.patch('/goods/:id', verifyUser, officeStaffOnly, updateGoods)
+router.post('/goods', verifyUser, officeStaffOnly, upload.single('images'), createGoods)
+router.patch('/goods/:id', verifyUser, officeStaffOnly, upload.single('images'), updateGoods)
 router.delete('/goods/:id', verifyUser, officeStaffOnly, deleteGoods)
 
 export default router
--- inventory-app-api/routes/UserRoute.js
+++ inventory-app-api/routes/UserRoute.js
@@ -7,13 +7,14 @@ import {
     deleteUser 
 } from "../controllers/Users.js"
 import { verifyUser, superAdminOnly } from "../middleware/AuthUser.js"
+import upload from "../middleware/Upload.js"
 
 const router = express.Router()
 
 router.get('/users', verifyUser, superAdminOnly, getUsers)
 router.get('/users/:id', verifyUser, superAdminOnly, getUserById)
-router.post('/users', verifyUser, superAdminOnly, createUser)
-router.patch('/users/:id', verifyUser, superAdminOnly, updateUser)
+router.post('/users', verifyUser, superAdminOnly, upload.single('images'), createUser)
+router.patch('/users/:id', verifyUser, superAdminOnly, upload.single('images'), updateUser)
 router.delete('/users/:id', verifyUser, superAdminOnly, deleteUser)
 
 export default router
